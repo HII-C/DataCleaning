1489ddef692702c6c5dd731de6f87f56cb41b3d3
Rohit Kumar <rohitkpa@gmail.com>
2018-09-19 11:41:55 -0700

    finished derived.visits_as_index

---
+++ DerivedVisitsAsIndex.py
@@ -0,0 +1,42 @@
+import pymysql as sql
+from getpass import getpass
+
+
+class DerivedVisitsAsIndex:
+    def __init__(self):
+        self.mimic_conn = None
+        self.mimic_cur = None
+        self.der_conn = None
+        self.der_cur = None
+
+    def connect_mimic_db(self, database):
+        self.mimic_conn = sql.connect(**database)
+        self.mimic_cur = self.mimic_conn.cursor()
+
+    def connect_der_db(self, database):
+        self.der_conn = sql.connect(**database)
+        self.der_cur = self.der_conn.cursor()
+
+    def create_derived(self, source, tbl):
+        mimic_source_str = f"""mimic.{source}"""
+        derived_tbl_str = f"""derived.{tbl}"""
+        create_str = """ROW_ID UNSIGNED INT, HADM_ID UNSIGNED INT"""
+        select_str = """SELECT ROW_ID, HADM_ID"""
+
+        exec_str = f"""CREATE TABLE {derived_tbl_str}{create_str} AS {select_str} FROM {mimic_source_str}"""
+
+        self.mimic_cur.execute(exec_str)
+        self.mimic_conn.commit()
+
+
+if __name__ == "__main__":
+    user = 'root'
+    pw = getpass(f'What is the password for the user {user}?\n')
+
+    mimic_db = {'user': user, 'db': 'mimic', 'host': 'db01.healthcreek.org', 'password': pw}
+    der_db = {'user': user, 'db': 'derived', 'host': 'db01.healthcreek.org', 'password': pw}
+
+    example = DerivedVisitsAsIndex()
+    example.connect_mimic_db(mimic_db)
+    example.connect_der_db(der_db)
+    example.create_derived("PRESCRIPTIONS", "visits_as_index")
