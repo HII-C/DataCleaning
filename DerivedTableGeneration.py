70eb77de1a28726dca43e9b94524bc960fdfda75
Rohit Kumar <rohitkpa@gmail.com>
2018-09-24 00:14:15 -0700

    added create_derived_loinc_labevents_min

--- DerivedTableGeneration.py
+++ DerivedTableGeneration.py
@@ -30,11 +30,12 @@ class DerivedTableGeneration:
         labitems_tbl_str = """mimic.D_LABITEMS"""
         derived_tbl_str = f"""derived.{tbl}"""
         create_str = """ROW_ID UNSIGNED INT, SUBJECT_ID UNSIGNED INT, HADM_ID UNSIGNED INT, LOINC_CODE VARCHAR(7), 
-        CHARTTIME TIMESTAMP(0), VALUE VARCHAR(200), VALUENUM DOUBLE, VALUEUOM VARCHAR(20), FLAG VARCHAR(20) """
+        CHARTTIME TIMESTAMP(0), VALUE VARCHAR(200), VALUENUM DOUBLE, VALUEUOM VARCHAR(20), FLAG VARCHAR(20)"""
         select_labevents_str = """SELECT ROW_ID, SUBJECT_ID, HADM_ID, CHARTTIME, VALUE, VALUENUM, VALUEUOM, FLAG"""
         select_labitems_str = """SELECT LOINC_CODE"""
 
-        exec_str = f"""CREATE TABLE {derived_tbl_str}{create_str} AS {select_labevents_str} FROM {labevents_tbl_str} UNION {select_labitems_str} FROM {labitems_tbl_str}"""
+        exec_str = f"""CREATE TABLE {derived_tbl_str}{create_str} AS {select_labevents_str} FROM {labevents_tbl_str} 
+        UNION {select_labitems_str} FROM {labitems_tbl_str}"""
         self.connection.mimic_cur.execute(exec_str)
         self.connection.mimic_conn.commit()
 
@@ -49,6 +50,20 @@ class DerivedTableGeneration:
         self.connection.mimic_cur.execute(exec_str)
         self.connection.mimic_conn.commit()
 
+    def create_derived_loinc_labevents_min(self, tbl):
+        labevents_tbl_str = """mimic.LABEVENTS"""
+        labitems_tbl_str = """mimic.D_LABITEMS"""
+        derived_tbl_str = f"""derived.{tbl}"""
+        create_str = """ROW_ID UNSIGNED INT, SUBJECT_ID UNSIGNED INT, HADM_ID UNSIGNED INT, LOINC_CODE VARCHAR(7),
+        FLAG VARCHAR(20)"""
+        select_labevents_str = """SELECT ROW_ID, SUBJECT_ID, HADM_ID, FLAG"""
+        select_labitems_str = """SELECT LOINC_CODE"""
+
+        exec_str = f"""CREATE TABLE {derived_tbl_str}{create_str} AS {select_labevents_str} from {labevents_tbl_str}
+        UNION {select_labitems_str} FROM {labitems_tbl_str}"""
+        self.connection.mimic_cur.execute(exec_str)
+        self.connection.mimic_conn.commit()
+
 
 if __name__ == "__main__":
     d_tbl_gen = DerivedTableGeneration()
@@ -59,3 +74,4 @@ if __name__ == "__main__":
     d_tbl_gen.create_derived_visits_as_index("PRESCRIPTIONS", "visits_as_index")
     d_tbl_gen.create_derived_loinc_labevents('loinc_labevents')
     # d_tbl_gen.create_derived_patient_has_diabetes("PRESCRIPTIONS", "patient_has_diabetes")
+    d_tbl_gen.create_derived_loinc_labevents_min('loinc_labevents_min')
